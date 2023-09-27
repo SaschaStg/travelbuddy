50d95110631b9eed5376fa63b600ae694f051863
Schmidt Sascha <ss532@hdm-stuttgart.de>
2023-09-27 13:42:36 +0200

    add default files, add main components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { CssBaseline, Grid, List } from '@material-ui/core';
+import Header from './components/Header/Header';
+import Map from './components/Map/Map';
+
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Map />
+                </Grid>
+            </Grid>
+
+        </>
+    );
+};
+
+export default App;
