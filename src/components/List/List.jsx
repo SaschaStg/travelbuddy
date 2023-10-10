140692eac98433ace22809fcc101e7c44c7815d3
Schmidt Sascha <ss532@hdm-stuttgart.de>
2023-10-10 16:17:42 +0200

    add list component and styles

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,45 @@
+import { CircularProgress, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from "@material-ui/core";
+import useStyles from "./styles";
+import { useState } from "react";
+
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
+
 const List = () => {
-    return <h1>List</h1>;
-  };
-  
-  export default List;
-  
+  const classes = useStyles();
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+
+  const place = [{ name: "Cool Place" }, { name: "Best Beer" }, { name: "Best Steak" }, { name: "Best Drinks" }, { name: "Coolest Food" }];
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid item key={i} xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default List;
