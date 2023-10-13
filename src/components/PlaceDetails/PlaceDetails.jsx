1a20bacc7120a85ed006a036b9e93ccb3b44dcf5
Schmidt Sascha <ss532@hdm-stuttgart.de>
2023-10-13 17:17:06 +0200

    add phone and website buttons to PlaceDetails

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,56 +1,74 @@
-import { Box, Card, CardContent, CardMedia, Chip, Typography } from "@material-ui/core";
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Chip, Typography } from "@material-ui/core";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
+import PhoneIcon from "@material-ui/icons/Phone";
 import useStyles from "./styles";
 
 const PlaceDetails = ({ place }) => {
   const classes = useStyles();
+  console.log("PLACES");
+  console.log(place);
 
-  return (
-    <Card elevation={6}>
-      <CardMedia
-        style={{ height: 350 }}
-        image={
-          place.photo
-            ? place.photo.images.large.url
-            : "https://images.unsplash.com/photo-1599795119678-63f52d65a5d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
-        }
-        title={place.name}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5">
-          {place.name}
-        </Typography>
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1">Price</Typography>
-          <Typography gutterBottom variant="subtitle1">
-            {place.price_level}
+  if (place.name) {
+    return (
+      <Card elevation={6}>
+        <CardMedia
+          style={{ height: 350 }}
+          image={
+            place.photo
+              ? place.photo.images.large.url
+              : "https://images.unsplash.com/photo-1599795119678-63f52d65a5d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
+          }
+          title={place.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            {place.name}
           </Typography>
-        </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1">Ranking</Typography>
-          <Typography gutterBottom variant="subtitle1">
-            {place.ranking}
-          </Typography>
-        </Box>
-        {place?.awards?.map((award) => (
-          <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
-            <img src={award.images.small} alt={award.display_name} />
-            <Typography variant="subtitle2" color="textSecondary">
-              {award.display_name}
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1">Price</Typography>
+            <Typography gutterBottom variant="subtitle1">
+              {place.price_level}
             </Typography>
           </Box>
-        ))}
-        {place?.cuisine?.map(({ name }) => (
-          <Chip key={name} size="small" label={name} className={classes.chip} />
-        ))}
-        {place?.address && (
-          <Typography gutterBottom variant="caption" color="textSecondary" className={classes.subtitle}>
-            <LocationOnIcon /> {place.address}
-          </Typography>
-        )}
-      </CardContent>
-    </Card>
-  );
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1">Ranking</Typography>
+            <Typography gutterBottom variant="subtitle1">
+              {place.ranking}
+            </Typography>
+          </Box>
+          {place?.awards?.map((award) => (
+            <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
+              <img src={award.images.small} alt={award.display_name} />
+              <Typography variant="subtitle2" color="textSecondary">
+                {award.display_name}
+              </Typography>
+            </Box>
+          ))}
+          {place?.cuisine?.map(({ name }) => (
+            <Chip key={name} size="small" label={name} className={classes.chip} />
+          ))}
+          {place?.address && (
+            <Typography gutterBottom variant="caption" color="textSecondary" className={classes.subtitle}>
+              <LocationOnIcon /> {place.address_obj.street1}, {place.address_obj.postalcode} {place.address_obj.city}
+            </Typography>
+          )}
+          {place?.phone && (
+            <Typography gutterBottom variant="caption" color="textSecondary" className={classes.subtitle}>
+              <PhoneIcon /> {place.phone}
+            </Typography>
+          )}
+          <CardActions>
+            <Button size="small" color="primary" onClick={() => window.open(place.web_url, "_blank")}>
+              Trip Advisor
+            </Button>
+            <Button size="small" color="primary" onClick={() => window.open(place.website, "_blank")}>
+              Website
+            </Button>
+          </CardActions>
+        </CardContent>
+      </Card>
+    );
+  }
 };
 
 export default PlaceDetails;
